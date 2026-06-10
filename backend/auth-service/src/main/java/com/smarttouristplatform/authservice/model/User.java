0b0e0a861036faf4895eb289184fa5bc25eb8399
DESHAN K.D.M <130528796+DHARAKA-METH@users.noreply.github.com>
2026-06-10 18:57:51 +0530

    implement databse connecrion

---
+++ backend/auth-service/src/main/java/com/smarttouristplatform/authservice/model/User.java
@@ -0,0 +1,19 @@
+package com.smarttouristplatform.authservice.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "users")
+public class User {
+    @Id
+    private String id;
+    private String username;
+    private String email;
+}
