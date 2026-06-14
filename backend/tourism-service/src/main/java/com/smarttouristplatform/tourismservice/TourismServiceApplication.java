58d4a1f016d17eea81a29745f1ef648ef30ef6cc
DESHAN K.D.M <130528796+DHARAKA-METH@users.noreply.github.com>
2026-06-14 16:21:30 +0530

    inizialize project

---
+++ backend/tourism-service/src/main/java/com/smarttouristplatform/tourismservice/TourismServiceApplication.java
@@ -0,0 +1,15 @@
+package com.smarttouristplatform.tourismservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourismServiceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TourismServiceApplication.class, args);
+        System.out.println("succes compileing TourismService ***********");
+    }
+
+}
---
+++ backend/tourism-service/src/test/java/com/smarttouristplatform/tourismservice/TourismServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarttouristplatform.tourismservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourismServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
